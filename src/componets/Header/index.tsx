1cf9cb3acdb1756fb71efa4835726f779e0f0145
William <williamfs133@gmail.com>
2023-09-21 11:11:16 -0300

    responsividade do menu, concluída

--- src/componets/Header/index.tsx
+++ src/componets/Header/index.tsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom';
 
-import { HeaderBar, Links, LinkItem, LinkCart } from './styles';
+import {
+    HeaderBar,
+    Links,
+    LinkItem,
+    LinkCart,
+    Hamburguer,
+    HeaderRow,
+    NavMobile
+} from './styles';
 
 import logoHeader from '../../assets/images/logo.svg';
 import logoCarrinho from '../../assets/images/carrinho.svg';
@@ -8,35 +16,59 @@ import { openCart } from '../../store/reducers/cart';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { RootReducer } from '../../store';
+import { useState } from 'react';
 
 const Header = () => {
     const dispatch = useDispatch();
     const { itens } = useSelector((state: RootReducer) => state.cart);
 
+    const [navMenu, setNavMenu] = useState(false);
+
     return (
         <HeaderBar>
-            <div>
-                <Link to={'/'}>
-                    <img src={logoHeader} alt="Logo EPLAY" />
-                </Link>
-                <nav>
-                    <Links>
-                        <LinkItem>
-                            <Link to={'/categories'}>Categorias</Link>
-                        </LinkItem>
-                        <LinkItem>
-                            <a href="#">Novidades</a>
-                        </LinkItem>
-                        <LinkItem>
-                            <a href="#">Promoções</a>
-                        </LinkItem>
-                    </Links>
-                </nav>
-            </div>
-            <LinkCart onClick={() => dispatch(openCart())}>
-                {itens.length} - Produtos{' '}
-                <img src={logoCarrinho} alt="Carrinho icone" />
-            </LinkCart>
+            <HeaderRow>
+                <div>
+                    <Hamburguer onClick={() => setNavMenu(!navMenu)}>
+                        <span />
+                        <span />
+                        <span />
+                    </Hamburguer>
+                    <Link to={'/'}>
+                        <img src={logoHeader} alt="Logo EPLAY" />
+                    </Link>
+                    <nav>
+                        <Links>
+                            <LinkItem>
+                                <Link to={'/categories'}>Categorias</Link>
+                            </LinkItem>
+                            <LinkItem>
+                                <a href="#">Novidades</a>
+                            </LinkItem>
+                            <LinkItem>
+                                <a href="#">Promoções</a>
+                            </LinkItem>
+                        </Links>
+                    </nav>
+                </div>
+                <LinkCart onClick={() => dispatch(openCart())}>
+                    {itens.length}
+                    <span> - Produtos </span>
+                    <img src={logoCarrinho} alt="Carrinho icone" />
+                </LinkCart>
+            </HeaderRow>
+            <NavMobile className={navMenu ? 'isOpen' : ''}>
+                <Links>
+                    <LinkItem>
+                        <Link to={'/categories'}>Categorias</Link>
+                    </LinkItem>
+                    <LinkItem>
+                        <a href="#">Novidades</a>
+                    </LinkItem>
+                    <LinkItem>
+                        <a href="#">Promoções</a>
+                    </LinkItem>
+                </Links>
+            </NavMobile>
         </HeaderBar>
     );
 };
--- src/componets/Header/styles.ts
+++ src/componets/Header/styles.ts
@@ -1,36 +1,69 @@
 import styled from 'styled-components';
 import { Cores, breakPoints } from '../../styles';
 
+export const Links = styled.ul`
+    display: flex;
+    margin-left: 40px;
+
+    @media (max-width: ${breakPoints.tablet}) {
+        margin-left: 0;
+        display: block;
+    }
+`;
+
 export const HeaderBar = styled.header`
     background-color: ${Cores.cinza};
     padding: 24px;
     border-radius: 16px;
     margin-bottom: 80px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    div {
-        display: flex;
-        align-items: center;
-    }
 
     a {
         font-weight: bold;
     }
-
-    @media (max-width: ${breakPoints.desktop}) {
-        display: none;
-    }
 `;
 
-export const Links = styled.ul`
-    display: flex;
-    margin-left: 40px;
+export const NavMobile = styled.div`
+    display: none;
+    transition: 05s;
+
+    &.isOpen {
+        display: block;
+        transition: 05s;
+    }
 `;
 
 export const LinkItem = styled.li`
     margin-right: 16px;
+
+    @media (max-width: ${breakPoints.tablet}) {
+        margin-left: 0;
+
+        a {
+            display: block;
+            padding: 16px 0;
+            text-align: center;
+        }
+    }
+`;
+
+export const HeaderRow = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > div {
+        display: flex;
+        align-items: center;
+
+        @media (max-width: ${breakPoints.tablet}) {
+            flex: 1;
+            justify-content: space-between;
+
+            ${Links} {
+                display: none;
+            }
+        }
+    }
 `;
 
 export const LinkCart = styled.a`
@@ -38,5 +71,27 @@ export const LinkCart = styled.a`
     cursor: pointer;
     img {
         margin-left: 16px;
+    }
+
+    @media (max-width: ${breakPoints.tablet}) {
+        span {
+            display: none;
+        }
+    }
+`;
+
+export const Hamburguer = styled.div`
+    width: 32px;
+
+    span {
+        height: 2px;
+        display: block;
+        width: 100%;
+        background-color: ${Cores.branca};
+        margin-bottom: 4px;
+    }
+
+    @media (min-width: ${breakPoints.tablet}) {
+        display: none;
     }
 `;
